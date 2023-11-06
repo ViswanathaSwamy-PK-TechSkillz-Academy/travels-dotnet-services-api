9d5c3a7164d17115951a39583ee5a5b73a1e103a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-06 22:57:55 +0530

    Update CountriesEndpoints.cs

--- src/Countries.API/Endpoints/CountriesEndpoints.cs
+++ src/Countries.API/Endpoints/CountriesEndpoints.cs
@@ -12,6 +12,17 @@ public static class CountriesEndpoints
     {
         var group = routes.MapGroup(CountriesRoutes.Prefix).WithTags(nameof(CreateCountryInfoDto));
 
+        _ = group.MapPost(CountriesRoutes.Root, async ([FromBody] CreateCountryInfoDto createCountryInfoDto, [FromServices] ICountriesBusiness countriesBusiness) =>
+        {
+            return Results.Ok(await countriesBusiness.AddCountry(createCountryInfoDto));
+
+        })
+          .AllowAnonymous()
+          .WithName("AddCountry")
+          .Produces<CountryInfoDto>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithOpenApi();
+
         _ = group.MapGet(CountriesRoutes.Root, async ([FromServices] ICountriesBusiness countriesBusiness) =>
         {
             return Results.Ok(await countriesBusiness.GetAllCountries());
