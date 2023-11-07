d917b9e798564be7fa7072d7b57a63acfe99cfc3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-07 09:53:57 +0530

    Update CreateCountryInfoDto.cs

--- src/Countries.Data/Dtos/CreateCountryInfoDto.cs
+++ src/Countries.Data/Dtos/CreateCountryInfoDto.cs
@@ -1,12 +1,18 @@
-﻿namespace Countries.Data.Dtos;
+﻿using System.Text.Json.Serialization;
+
+namespace Countries.Data.Dtos;
 
 public record CreateCountryInfoDto
 {
+    [JsonPropertyName("country_name")]
     public string? CountryName { get; set; }
 
+    [JsonPropertyName("capital_state")]
     public string? CapitalState { get; set; }
 
+    [JsonPropertyName("national_bird")]
     public string? NationalBird { get; set; }
 
+    [JsonPropertyName("country_population")]
     public long CountryPopulation { get; set; }
 }
